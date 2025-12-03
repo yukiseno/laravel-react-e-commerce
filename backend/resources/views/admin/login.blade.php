29b95549761666bcfd43af2249231728d8e31196
Yuki Seno <yukiseno1@gmail.com>
2025-12-04 10:55:09 +1300

    Working on Login page

---
+++ backend/resources/views/admin/login.blade.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-6 mx-auto ">
+                @session('error')
+                    <div class="alert alert-danger my-2">
+                        {{ session('error') }}
+                    </div>
+                @endsession
+                <div class="card p-5">
+                    <div class="card-header text-center bg-white border-0">
+                        <h3 class="mt-2">Login</h3>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('admin.auth') }}" method="post">
+                            @csrf
+                            <div class="form-floating mb-3">
+                                <input type="email" class="form-control" id="floatingInput" name="email"
+                                    placeholder="name@example.com">
+                                <label for="floatingInput">Email address</label>
+                                @error('email')
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control" id="floatingPassword" name="password"
+                                    placeholder="Password">
+                                <label for="floatingPassword">Password</label>
+                                @error('password')
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="mb-2">
+                                <button class="btn btn-lg btn-dark">Log In</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+        </script>
+</body>
+
+</html>
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AdminController::class, 'login'])->name('admin.login');
+Route::post('admin/auth', [AdminController::class, 'auth'])->name('admin.auth');
+Route::get('/admin/bashboard', [AdminController::class, 'index'])->name('admin.index');
