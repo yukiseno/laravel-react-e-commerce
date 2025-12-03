b23254e36058ee70891122b1b671f78b7e5dd77e
Yuki Seno <yukiseno1@gmail.com>
2025-12-04 10:09:14 +1300

    Factories and Seeders

--- backend/database/seeders/AdminSeeder.php
+++ backend/database/seeders/AdminSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Admin;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class AdminSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Admin::factory(1)->create();
     }
 }
