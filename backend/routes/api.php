2d6906cdb97385ddf0aa17f27abab598d58ed8b5
Yuki Seno <yukiseno1@gmail.com>
2025-12-04 17:12:24 +1300

    api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//products routes
+Route::get('products', [ProductController::class, 'index']);
+Route::get('products/{color}/color', [ProductController::class, 'filterProductsByColor']);
+Route::get('products/{size}/size', [ProductController::class, 'filterProductsBySize']);
+Route::get('products/{searchTerm}/find', [ProductController::class, 'findProductsByTerm']);
+Route::get('product/{product}/show', [ProductController::class, 'show']);
