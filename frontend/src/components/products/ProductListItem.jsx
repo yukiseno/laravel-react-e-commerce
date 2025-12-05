97160cd07c33a6049cbe661935e79c4d85510c57
Yuki Seno <yukiseno1@gmail.com>
2025-12-05 13:10:27 +1300

    to show product name and price

--- frontend/src/components/products/ProductListItem.jsx
+++ frontend/src/components/products/ProductListItem.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function ProductListItem({ product }) {
-  return <div>{product.name}</div>;
+  return (
+    <Link to={`/product/${product.slug}`}>
+      <img src={product.thumbnail} alt={product.name} />
+      <div>{product.name}</div>
+      <div>{product.price}</div>
+    </Link>
+  );
 }
