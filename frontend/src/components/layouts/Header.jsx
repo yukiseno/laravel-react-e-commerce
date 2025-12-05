fc1b1e783a2841f20101c2e098b298581f8d846e
Yuki Seno <yukiseno1@gmail.com>
2025-12-05 13:30:02 +1300

    to use font awesome

--- frontend/src/components/layouts/Header.jsx
+++ frontend/src/components/layouts/Header.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="">
+      <Link to="/">
+        <FontAwesomeIcon icon={faHouse} />
+      </Link>
+    </div>
+  );
 }
