22c448cbc08fdd796bdbaeb8bb3093e808eb9451
Yuki Seno <yukiseno1@gmail.com>
2025-12-05 13:52:00 +1300

    to show more information

--- frontend/src/components/products/ProductListItem.jsx
+++ frontend/src/components/products/ProductListItem.jsx
@@ -6,6 +6,33 @@ export default function ProductListItem({ product }) {
       <img src={product.thumbnail} alt={product.name} />
       <div>{product.name}</div>
       <div>{product.price}</div>
+      <div className="">
+        {product.sizes?.map((size) => (
+          <span key={size.id} className="">
+            <small>{size.name}</small>
+          </span>
+        ))}
+      </div>
+      <div>
+        {product.status == 1 ? (
+          <span className="badge bg-success p-2">In Stock</span>
+        ) : (
+          <span className="badge bg-danger p-2">Out of Stock</span>
+        )}
+      </div>
+      <div className="">
+        {product.colors?.map((color) => (
+          <div
+            key={color.id}
+            className=""
+            style={{
+              backgroundColor: color.name.toLowerCase(),
+              height: "20px",
+              width: "20px",
+            }}
+          ></div>
+        ))}
+      </div>
     </Link>
   );
 }
