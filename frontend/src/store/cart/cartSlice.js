30d9e0ae2894ab3a16d1c5398674d2b90c2b3a26
Yuki Seno <yukiseno1@gmail.com>
2025-12-06 15:25:35 +1300

    increase, decrease, removeitem

--- frontend/src/store/cart/cartSlice.js
+++ frontend/src/store/cart/cartSlice.js
@@ -25,9 +25,42 @@ export const cartSlice = createSlice({
         toast.success("Product already added to your cart");
       }
     },
-    incrementQ(state, action) {},
-    decrementQ(state, action) {},
-    removeFromCart(state, action) {},
+    incrementQ(state, action) {
+      const item = action.payload;
+      let productItem = state.cartItems.find(
+        (product) =>
+          product.product_id === item.product_id &&
+          product.color === item.color &&
+          product.size === item.size
+      );
+      if (productItem.qty === productItem.maxQty) {
+        toast.info(`Only ${productItem.maxQty} available`);
+      } else {
+        productItem.qty += 1;
+      }
+    },
+    decrementQ(state, action) {
+      const item = action.payload;
+      let productItem = state.cartItems.find(
+        (product) =>
+          product.product_id === item.product_id &&
+          product.color === item.color &&
+          product.size === item.size
+      );
+      productItem.qty -= 1;
+      if (productItem.qty === 0) {
+        state.cartItems = state.cartItems.filter(
+          (product) => product.ref !== item.ref
+        );
+      }
+    },
+    removeFromCart(state, action) {
+      const item = action.payload;
+      state.cartItems = state.cartItems.filter(
+        (product) => product.ref !== item.ref
+      );
+      toast.success("Product removed from your cart");
+    },
   },
 });
 
