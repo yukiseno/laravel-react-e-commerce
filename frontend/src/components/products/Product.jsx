234ec6cf9d92b8b582de9c4445aef1fd099362e0
Yuki Seno <yukiseno1@gmail.com>
2025-12-06 15:25:11 +1300

    bug fix, qby should be number

--- frontend/src/components/products/Product.jsx
+++ frontend/src/components/products/Product.jsx
@@ -132,7 +132,7 @@ export default function Product() {
                   min={1}
                   max={product?.qty > 1 ? product.qty : 1}
                   value={qty}
-                  onChange={(e) => setQty(e.target.value)}
+                  onChange={(e) => setQty(Number(e.target.value))}
                   className="w-full px-3 py-2 border rounded-lg focus:ring focus:ring-gray-300"
                   placeholder="Qty"
                 />
