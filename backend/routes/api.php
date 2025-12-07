303a944be333f477c2dd576765f1a5efe5902789
Yuki Seno <yukiseno1@gmail.com>
2025-12-08 09:52:23 +1300

    routes for api/user

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,27 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\ProductController;
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', function (Request $request) {
+        return [
+            'user' => UserResource::make($request->user()),
+            'access_token' => $request->bearerToken()
+        ];
+    });
+    Route::post('user/logout', [UserController::class, 'logout']);
+    Route::put('user/profile/update', [UserController::class, 'UpdateUserProfile']);
+});
 //products routes
 Route::get('products', [ProductController::class, 'index']);
 Route::get('products/{color}/color', [ProductController::class, 'filterProductsByColor']);
 Route::get('products/{size}/size', [ProductController::class, 'filterProductsBySize']);
 Route::get('products/{searchTerm}/find', [ProductController::class, 'findProductsByTerm']);
 Route::get('product/{product}/show', [ProductController::class, 'show']);
+//user routes
+Route::post('user/register', [UserController::class, 'store']);
+Route::post('user/login', [UserController::class, 'auth']);
