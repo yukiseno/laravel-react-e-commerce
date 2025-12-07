d58bba2cd3b394c964b0f79ff9b153e29a9ca724
Yuki Seno <yukiseno1@gmail.com>
2025-12-08 12:33:20 +1300

    safty improvement

--- frontend/src/components/common/ValidationErrors.jsx
+++ frontend/src/components/common/ValidationErrors.jsx
@@ -1,6 +1,6 @@
-export default function ValidationErrors({ errors, field }) {
-  if (!errors?.[field]) return null;
-  return errors?.[field]?.map((error, index) => (
+export default function ValidationErrors({ errors = {}, field }) {
+  if (!Array.isArray(errors[field])) return null;
+  return errors[field].map((error, index) => (
     <p key={index} className="mt-1 text-sm text-red-600">
       {error}
     </p>
