a9175fca8a671f5dec2cc16450b49ca5b2b056a9
Yuki Seno <yukiseno1@gmail.com>
2025-12-08 17:20:34 +1300

    to use persist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,15 +6,18 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import "react-image-gallery/styles/css/image-gallery.css";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { store, persistor } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <div className="container mx-auto px-4">
-        <ToastContainer position="top-right" />
-        <App />
-      </div>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="container mx-auto px-4">
+          <ToastContainer position="top-right" />
+          <App />
+        </div>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import cartReducer from "./cart/cartSlice";
 import userReducer from "./user/userSlice";
 
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    user: userReducer,
-  },
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  user: userReducer,
 });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
