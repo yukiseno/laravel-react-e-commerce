756dbb3967c46efbd3fe81e5f7b371a02dc1d826
Yuki Seno <yukiseno1@gmail.com>
2025-12-09 11:21:41 +1300

    order_items table

--- backend/database/migrations/2025_12_08_221718_create_order_items_table.php
+++ backend/database/migrations/2025_12_08_221718_create_order_items_table.php
@@ -13,7 +13,23 @@ return new class extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('order_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+
+            $table->foreignId('color_id')->constrained()->restrictOnDelete();
+            $table->foreignId('size_id')->constrained()->restrictOnDelete();
+
+            $table->string('color_name');
+            $table->string('size_name');
+
+            $table->integer('qty');
+            $table->integer('price');
+            $table->integer('subtotal');
+
             $table->timestamps();
+
+            $table->index('order_id');
         });
     }
 
