c26e790484922e70fa5df29fc322a9c09737d5c4
Yuki Seno <yukiseno1@gmail.com>
2025-12-08 16:46:48 +1300

    working on stripe

--- frontend/src/components/checkout/PayByStripe.jsx
+++ frontend/src/components/checkout/PayByStripe.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Stripe from "./Stripe";
 
 export default function PayByStripe() {
-  return <div>PayByStripe</div>;
+  const { isLoggedIn } = useSelector((state) => state.user);
+  const { cartItems } = useSelector((state) => state.cart);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoggedIn) navigate("/login");
+    if (!cartItems.length) navigate("/");
+  }, [isLoggedIn, cartItems, navigate]);
+  return (
+    <div className="my-5 flex justify-center px-4">
+      <div className="w-full md:w-1/2">
+        <Stripe />
+      </div>
+    </div>
+  );
 }
