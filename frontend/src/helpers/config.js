a2ec6306ebde8f3c288ba87c5cd8d7606511ab1f
Yuki Seno <yukiseno1@gmail.com>
2025-12-12 11:10:24 +1300

    for the production page

--- frontend/src/helpers/config.js
+++ frontend/src/helpers/config.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
+
 export const axiosRequest = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: baseURL + "/api/",
 });
 
 export const getConfig = (token, contentType) => {
